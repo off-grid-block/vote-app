d11f038a83111e0100d18639e0d4abf81f2ae969
Brian Li <brianli1117@gmail.com>
2020-04-29 12:45:12 -0400

    register vote app with DEON service

--- vote/apps.py
+++ vote/apps.py
@@ -1,5 +1,16 @@
 from django.apps import AppConfig
+from deon_apps.settings import API_URL
+
+import requests
 
 
 class VoteConfig(AppConfig):
     name = 'vote'
+
+    def ready(self):
+        resp = requests.post(
+            f'{API_URL}/application',
+            json={'name': 'Voting', 'secret': 'kerapwd', 'type': 'user'}
+        )
+        if resp.status_code >= 300:
+            raise RuntimeError('Failed to register  app with DEON service')
