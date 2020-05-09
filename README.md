8ec4dbc76868c1134d8bcd805bb4409365370580
Brian Li <brianli1117@gmail.com>
2020-05-09 12:36:39 -0400

    add instructions to readme

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# vote-app
+# DEON Service - Voting
+
+This is the repository for an example application deployed on top of the DEON service.
+
+## Getting Started
+
+Clone this repo and run:
+
+```bash
+cd vote-app
+pipenv shell
+```
+
+## Test Deployment
+
+To run the app in a test environment, use the following command inside the cloned directory:
+
+```bash
+python manage.py runserver [port]
+```
+
+## Manage App
+
+View the admin page at [URL]/admin.
--- vote/apps.py
+++ vote/apps.py
@@ -7,10 +7,10 @@ import requests
 class VoteConfig(AppConfig):
     name = 'vote'
 
-    def ready(self):
-        resp = requests.post(
-            f'{API_URL}/application',
-            json={'name': 'Voting', 'secret': 'kerapwd', 'type': 'user'}
-        )
-        if resp.status_code >= 300:
-            raise RuntimeError('Failed to register  app with DEON service')
+    # def ready(self):
+    #     resp = requests.post(
+    #         f'{API_URL}/application',
+    #         json={'name': 'Voting', 'secret': 'kerapwd', 'type': 'user'}
+    #     )
+    #     if resp.status_code >= 300:
+    #         raise RuntimeError('Failed to register  app with DEON service')
